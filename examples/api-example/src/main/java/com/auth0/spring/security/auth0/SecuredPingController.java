e4d6c77868649f6aec3bad2170038133022a4c01
Hubert Sugeng <huberts+github@gmail.com>
2015-04-28 10:43:53 -0400

    Adding to the sample how Spring Security normally obtains the UserDetails (which in this case is really Auth0UserDetails). This always returns UNKOWNUN_USER after authenticating with an Angular.JS front end.

--- examples/api-example/src/main/java/com/auth0/spring/security/auth0/SecuredPingController.java
+++ examples/api-example/src/main/java/com/auth0/spring/security/auth0/SecuredPingController.java
@@ -1,16 +1,26 @@
 package com.auth0.spring.security.auth0;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SecuredPingController {
-
+	private final Log logger = LogFactory.getLog(getClass());
 
 	@RequestMapping(value = "/secured/ping")
 	@ResponseBody
 	public String securedPing() {
-		return "All good. You only get this message if you're authenticated";
+		Object principalObject = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetails userDetails = null;
+        if(principalObject instanceof UserDetails){
+            userDetails = (UserDetails) principalObject;
+        }
+        logger.info("Username: " + userDetails.getUsername());
+		return "All good. You only get this message if you're authenticated. " + userDetails;
 	}
 }
